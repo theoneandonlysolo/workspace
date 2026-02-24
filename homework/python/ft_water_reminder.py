3bdb87e310012413be2febd054f064ce21a9484a
Solo <solo@192.168.11.121>
2026-02-24 01:02:25 +0000

    completed all 4 exercices

---
+++ homework/python/ft_water_reminder.py
@@ -0,0 +1,8 @@
+def ft_water_reminder():
+    days = int(input("Days since last watering: "))
+    if days > 2:
+        print("Water the plants!")
+    else: 
+        print("Plants are fine")
+
+ft_water_reminder()
