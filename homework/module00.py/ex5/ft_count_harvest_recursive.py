0b31a5a5e56f683ee6122f2dbc01be8bab0fc10d
Solo <solo@192.168.11.107>
2026-03-08 21:20:57 +0000

    update

--- homework/module00.py/ex5/ft_count_harvest_recursive.py
+++ homework/module00.py/ex5/ft_count_harvest_recursive.py
@@ -1,10 +1,13 @@
 import time
 
-def ft_count_harvest_recursive():
-    daysuntil = int(input("Days until harvest: "))
-    i = 1
-    while i <= daysuntil:
+def ft_count_harvest_recursive(days):
+    if days == 0:
+        print("Harvest time!")
+    else:
+        print(f"{days} day(s) until harvest.")
         time.sleep(0.5)
-        print(f"Day {i}")
-        i += 1
-    print("Harvest time!")
+        ft_count_harvest_recursive(days - 1)
+
+    
+n = int(input("Days until harvest: "))
+ft_count_harvest_recursive(n)
